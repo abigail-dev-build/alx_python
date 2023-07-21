6e6e9218d59e4d31bbb2370a37cd4259aceb029c
Abigail Folarin <folarinabigail2020@gmail.com>
2023-07-21 01:24:35 +0100

    fix-task-3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,7 +7,7 @@ LAST_DIGIT = abs(number) % 10
 
 if LAST_DIGIT > 5:
     print(f"Last digit of {number}, is {LAST_DIGIT} and is greater than 5")
-elif LAST_DIGIT == 0 :
-    print(f"Last digit of {number}, is {LAST_DIGIT} and is 0")
-else:
+elif 0 < LAST_DIGIT < 6  :
     print(f"Last digit of {number}, is {LAST_DIGIT} and is less than 6 and not 0")
+else:
+    print(f"Last digit of {number}, is {LAST_DIGIT} and is 0")
