b5810d310b2d1b5f1657ff783c43d83515d73f2b
Abigail Folarin <folarinabigail2020@gmail.com>
2023-07-26 16:13:40 +0100

    rewrite-task-3

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -4,5 +4,6 @@ def safe_print_division(a, b):
     except (ZeroDivisionError, ValueError, TypeError):
         result = None
     finally: 
-            print("Inside result: {}".format(result))
-            print("{:d} / {:d} = {}".format(a, b, result))
+            if result is not None:
+                print("Inside result: {}".format(result))
+                print("{:d} / {:d} = {}".format(a, b, result))
