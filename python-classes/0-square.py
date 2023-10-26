876a4699320eeea581ce90ece5d3ca0a79be0cb8
Abigail Folarin <folarinabigail2020@gmail.com>
2023-10-26 21:09:07 +0100

    task-0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,20 +1,16 @@
+"""
+This module is a definition of a class Square that represents a square.
+
+
+Methods:
+    __init__(self, size): Initializes an instance or object of the class
+"""
 class Square():
     '''Defining the size of  a square'''
     def __init__(self, size):
-        self.size = size
+        """Initialize a square instance
+            Args:
+                size(int): the size of the square
+                """
+        self.__size = size
 
-    # '''Getting the size of  a square'''
-    # def get_size(self):
-    #     return self.size
-
-    # '''Defining a square by size'''
-    # def set_size(self, size):
-    #     self.size = size
-
-    # '''Defining a square by area'''
-    # def area(self):
-    #     return self.size ** 2
-
-    # '''Defining a square by perimeter'''
-    # def perimeter(self):
-    #     return 4 * self.size
