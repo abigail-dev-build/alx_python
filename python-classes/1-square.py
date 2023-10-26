f180a3bca8811dcc2c3f9f65c53b6f5ab737ce24
Abigail Folarin <folarinabigail2020@gmail.com>
2023-10-26 21:13:46 +0100

    task-1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,24 +1,17 @@
-class Square:
+"""
+This module is a definition of a class Square that represents a square.
+
+
+Methods:
+    __init__(self, size): Initializes an instance or object of the class
+"""
+class Square():
     '''Defining the size of  a square'''
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.size = size
-
-    '''Getting the size of  a square'''
-    def get_size(self):
-        return self.size
-
-    '''Defining a square by size'''
-    def set_size(self, size):
-        self.size = size
-
-    '''Defining a square by area'''
-    def area(self):
-        return self.size ** 2
-
-    '''Defining a square by perimeter'''
-    def perimeter(self):
-        return 4 * self.size
+         """Initialize a square instance
+            Args: size(int): the size of the square"""
+    if size != int(size):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,3 +1,10 @@
+"""
+This module is a definition of a class Square that represents a square.
+
+
+Methods:
+    __init__(self, size): Initializes an instance or object of the class
+"""
 class Square:
     '''Defining the size of  a square'''
     def __init__(self, size=0):
