90de66e021e6cfb95c3b8679367e86a9f5bbfbfe
Abigail Folarin <folarinabigail2020@gmail.com>
2023-10-26 21:05:01 +0100

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,20 +1,20 @@
-class Square:
+class Square():
     '''Defining the size of  a square'''
     def __init__(self, size):
         self.size = size
 
-    '''Getting the size of  a square'''
-    def get_size(self):
-        return self.size
+    # '''Getting the size of  a square'''
+    # def get_size(self):
+    #     return self.size
 
-    '''Defining a square by size'''
-    def set_size(self, size):
-        self.size = size
+    # '''Defining a square by size'''
+    # def set_size(self, size):
+    #     self.size = size
 
-    '''Defining a square by area'''
-    def area(self):
-        return self.size ** 2
+    # '''Defining a square by area'''
+    # def area(self):
+    #     return self.size ** 2
 
-    '''Defining a square by perimeter'''
-    def perimeter(self):
-        return 4 * self.size
+    # '''Defining a square by perimeter'''
+    # def perimeter(self):
+    #     return 4 * self.size
